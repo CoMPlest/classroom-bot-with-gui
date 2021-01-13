191298d474456b04cef32f75ae902e152e279ef2
CoMPlest <szekelyhidi.aron@gmail.com>
2021-01-13 11:54:25 +0100

    v1.0.6

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow, nativeTheme, Tray, Menu  } = require('electron');
 const fs = require("fs");
 
 const cookiesPath = "./cookies.json";
-let tray = null;
+//let tray = null;
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -13,23 +13,24 @@ function createWindow () {
     }
   })
   nativeTheme.themeSouce = 'dark';
-  //win.setMenu(null);
+  win.setMenu(null);
   win.loadFile('./src/index.html');
 }
 
 app.whenReady().then(() => {
-  tray = new Tray('./icon_big.png')
-  const contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio' },
-    { label: 'Item2', type: 'radio' },
-    { label: 'Item3', type: 'radio', checked: true },
-    { label: 'Item4', type: 'radio' }
-  ])
-  tray.setToolTip('This is my application.')
-  tray.setContextMenu(contextMenu)
+  require('./src/updater')();
+  // tray = new Tray()
+  // const contextMenu = Menu.buildFromTemplate([
+  //   { label: 'Item1', type: 'radio' },
+  //   { label: 'Item2', type: 'radio' },
+  //   { label: 'Item3', type: 'radio', checked: true },
+  //   { label: 'Item4', type: 'radio' }
+  // ])
+  // tray.setToolTip('This is my application.')
+  // tray.setContextMenu(contextMenu)
 
   try {
-    if (!fs.existsSync(cookiesPath) || JSON.parse(fs.readFileSync(cookiesPath))[1].expiry > Date.now()) {
+    if (!fs.existsSync(cookiesPath) || JSON.parse(fs.readFileSync(cookiesPath)).filter(obj => { return obj.name === 'SIDCC' })[0].expiry > Date.now()) {
       require('./src/login')(app);
     } else {
       require('./src/taskEventHandler')(app);
